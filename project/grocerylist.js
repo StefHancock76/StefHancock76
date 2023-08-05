11927b284f706024dd2ad315d4bb8b9a2a075d83
StefHancock76 <stefshubert76@gmail.com>
2023-08-05 16:07:23 -0500

    Working on project

--- project/grocerylist.js
+++ project/grocerylist.js
@@ -1,11 +1,11 @@
 let groceryList = document.querySelector('#grocery_list');
-const addToListButton = document.querySelector('#add_to_list');
-const groceryListInput = document.querySelector("#grocery_list_input");
-const groceryQtyInput = document.querySelector("#quantity_input");
-
+let addToListButton = document.querySelector('#add_to_list');
+let groceryListInput = document.querySelector("#grocery_list_input");
+let editListName = document.querySelector('#edit_list_name');
+let groceryQtyInput = document.querySelector("#quantity_input");
 let listName = document.querySelector('#list_name');
-let listNameInput = document.querySelector('#list_name_input');
-const editListName = document.querySelector('#edit_list_name');
+let listNameInput = document.querySelector('#list_item');
+
 
 editListName.addEventListener('click', () => {
     if (listNameInput.style.visibility = 'hidden') {
@@ -20,12 +20,19 @@ editListName.addEventListener('click', () => {
 
 let addToList = () => {
     if (groceryListInput.value !== '') {
-        let li = document.createElement('li');
-        li.textContent = groceryListInput.value;
-        groceryList.appendChild(li);
+        let tr = document.createElement('tr');
+        tr.textContent = groceryListInput.value;
+        groceryList.appendChild(tr);
         groceryListInput.value = '';
     }
+    if (groceryQtyInput.value !== '') {
+        let th = document.createElement('th');
+        th.textContent = groceryQtyInput.value;
+        groceryList.appendChild(th);
+        groceryQtyInput.value = '';
+    }
 }
+
 
 addToListButton.addEventListener('click', () => {
     addToList();
